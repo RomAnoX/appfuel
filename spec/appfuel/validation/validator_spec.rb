ed80a758d8c6d9165c08f37d6c44d197ea8aa5d4
Robert Scott-Buccleuch <rsb.code@gmail.com>
2017-04-21 07:01:39 -0700

    finished tests for validator

--- spec/appfuel/validation/validator_spec.rb
+++ spec/appfuel/validation/validator_spec.rb
@@ -1,31 +1,77 @@
 module Appfuel::Validation
-  RSpec.describe '#initialize' do
-    it 'requires a name and a schema' do
-      name = 'my_validator'
-      schema = double('some schema')
-      allow(schema).to receive(:call)
+  RSpec.describe Appfuel::Validation do
+    describe '#initialize' do
+      it 'requires a name and a schema' do
+        name = 'my_validator'
+        schema = mock_schema
 
-      validator = Validator.new(name, schema)
-      expect(validator.name).to eq(name)
-      expect(validator.schema).to eq(schema)
+        validator = Validator.new(name, schema)
+        expect(validator.name).to eq(name)
+        expect(validator.schema).to eq(schema)
+      end
+
+      it 'defaults fail fast to false' do
+        validator = Validator.new('my-name', mock_schema)
+        expect(validator.fail_fast?).to be false
+      end
+
+      it 'fails when schema does not implement call' do
+        schema = double('some schema')
+        msg = 'schema must implement :call'
+        expect {
+          Validator.new('my-name', schema)
+        }.to raise_error(ArgumentError, msg)
+      end
     end
 
-    it 'defaults fail fast to false' do
-      name = 'my_validator'
-      schema = double('some schema')
-      allow(schema).to receive(:call)
-
-      validator = Validator.new(name, schema)
-      expect(validator.fail_fast?).to be false
+    context '#enable_fast_fail' do
+      it 'toggles fail_fast to true' do
+        validator = Validator.new('some-name', mock_schema)
+        validator.enable_fail_fast
+        expect(validator.fail_fast?).to be true
+      end
     end
 
-    it 'fails when schema does not implement call' do
-      name = 'my_validator'
-      schema = double('some schema')
-      msg = 'schema must implement :call'
-      expect {
-        Validator.new(name, schema)
-      }.to raise_error(ArgumentError, msg)
+    context '#disable_fast_fail' do
+      it 'toggles fail_fast to true' do
+        validator = Validator.new('some-name', mock_schema)
+        validator.enable_fail_fast
+        validator.disable_fail_fast
+        expect(validator.fail_fast?).to be false
+      end
+    end
+
+    context '#pipe?' do
+      it 'returns false beause it is not a pipe' do
+        validator = Validator.new('some-name', mock_schema)
+        expect(validator.pipe?).to be false
+      end
+    end
+
+    context '#call' do
+      it 'delegates to the dry schema' do
+        schema = double('schema')
+        inputs = {foo: 'bar'}
+
+        expect(schema).to receive(:call).with(inputs)
+        validator = Validator.new('some-name', schema)
+
+        validator.call(inputs)
+      end
+
+      it 'returns the results from validating the inputs' do
+        inputs = {foo: 'bar'}
+        output = 'some validation output'
+        schema = mock_schema(inputs, output)
+        validator = Validator.new('some-name', schema)
+        expect(validator.call(inputs)).to eq output
+      end
+    end
+
+    def mock_schema(inputs = {}, output = {})
+      schema = double('schema')
+      allow(schema).to receive(:call).with(inputs) { output }
+      schema
     end
   end
 end
