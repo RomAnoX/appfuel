3f52ef1cb128bd18075c9ef938f817d088958ff9
Robert Scott-Buccleuch <rsb.code@gmail.com>
2017-05-31 12:55:28 -0700

    adding more tests for repo

--- spec/appfuel/storage/repository/base_spec.rb
+++ spec/appfuel/storage/repository/base_spec.rb
@@ -8,16 +8,16 @@ module Appfuel::Repository
       end
     end
 
-    context 'container_class_type' do
+    context '.container_class_type' do
       it 'has a type of "repositories"' do
         expect(setup.container_class_type).to eq('repositories')
       end
     end
 
-    context 'create_mapper' do
+    context '.create_mapper' do
       it 'creates a repository mapper' do
         repo = setup
-        expect(repo.create_mapper).to be_an_instance_of(Mapper)
+        expect(repo.create_mapper).to be_an_instance_of(mapper_class)
       end
 
       it 'creates a repository mapper with a map' do
@@ -34,7 +34,24 @@ module Appfuel::Repository
       end
     end
 
-    context 'cache' do
+    context '.mapper' do
+      it 'creates a mapper when none exists' do
+        repo = setup
+        result = 'I am a faked out mapper instance'
+        expect(repo).to receive(:create_mapper).with(no_args) { result }
+
+        expect(repo.mapper).to eq(result)
+      end
+
+      it 'assigns a custom mapper' do
+        repo = setup
+        mapper = 'i am a mapper'
+        repo.mapper = mapper
+        expect(repo.mapper).to eq(mapper)
+      end
+    end
+
+    context '.cache' do
       it 'delegates the cache to the app_container' do
         container = build_container
         repo = setup(container)
@@ -42,6 +59,21 @@ module Appfuel::Repository
         expect(container).to receive(:[]).with(:repository_cache) { cache }
         expect(repo.cache).to eq(cache)
       end
+    end
+
+    context '#mapper' do
+      it 'delegates to the class method' do
+        repo_class = setup
+        repo = repo_class.new
+        result = 'some mapper'
+        expect(repo_class).to receive(:mapper).with(no_args) { result }
+        expect(repo.mapper).to eq(result)
+
+      end
+    end
+
+    def mapper_class
+      Mapper
     end
 
     def setup(container = Dry::Container.new, class_name = "FooApp::Bar::Fiz")
