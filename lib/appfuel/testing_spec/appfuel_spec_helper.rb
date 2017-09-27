4f781a450f68792c660bb203c62dde2bf1520377
Robert Scott-Buccleuch <rsb.code@gmail.com>
2017-09-27 16:35:33 -0700

    fixing spec helper

--- lib/appfuel/testing_spec/appfuel_spec_helper.rb
+++ lib/appfuel/testing_spec/appfuel_spec_helper.rb
@@ -5,13 +5,21 @@ require_relative 'helpers'
 RSpec.configure do |config|
   config.before(:each) do
 
-    @types  = Types.container
+    @types = Types.container
+    @app_container    = Appfuel.app_container
+    @default_app_name = Appfuel.default_app_name
+    @framework_container = Appfuel.framework_container
+
     Types.send(:instance_variable_set, :@container, @types.dup)
-    Appfuel.framework_container = Dry::Container.new
+    Types.send(
+      :instance_variable_set,
+      :@framework_container,
+      @framework_container.dup)
   end
 
   config.after(:each) do
     Types.send(:instance_variable_set, :@container, @types)
+    Appfuel.framework_container = @framework_container
   end
 
   config.include Appfuel::TestingSpec::Helpers
